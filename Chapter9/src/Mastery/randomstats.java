a0586f482a6351d7b547a6e329a34ec44620e70d
38250015 <38250015@L815WD220006.edu.cbe.ab.ca>
2026-01-16 10:11:52 -0700

    22

--- Chapter9/src/Mastery/randomstats.java
+++ Chapter9/src/Mastery/randomstats.java
@@ -3,3 +3,25 @@ package Mastery;
 public class randomstats {
 
 }
+import java.util.Random;
+
+public class RandomStats {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int[] count = new int[10];
+
+        // generate 500 random numbers
+        for (int i = 0; i < 500; i++) {
+            int num = rand.nextInt(10); // 0 to 9
+            count[num]++;
+        }
+
+        // print results
+        System.out.println("Number\tOccurrences");
+        for (int i = 0; i < count.length; i++) {
+            System.out.println(i + "\t" + count[i]);
+        }
+    }
+}
